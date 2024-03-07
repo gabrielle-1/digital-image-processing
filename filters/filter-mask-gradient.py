c296e2703e8221f3b3f0a88b4b80cccc691418c1
gabrielle-1 <gabriellerodriguesmd@gmail.com>
2024-03-07 09:09:52 -0300

    Added filter prewitt and rotation and translation

--- filters/filter-mask-gradient.py
+++ filters/filter-mask-gradient.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2 as cv
 
+# Gradient Basic
 def gradiant(x, y, img):
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
@@ -14,7 +15,7 @@ def gradiant(x, y, img):
     grad_mid = cv.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
     return grad_mid
 
-
+# Gradient Sobel
 def gradiant_sobel(img):
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     ddepth = cv.CV_16S
@@ -28,6 +29,23 @@ def gradiant_sobel(img):
     # grad_sobel = cv.addWeighted(abs_grad_x, 0.2, abs_grad_y, 0.7, 0)
     grad_sobel = cv.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
     return grad_sobel
+
+# Gradient Prewitt
+def gradiant_prewitt(x, y, img):
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+    grad_x = cv.filter2D(gray, -1, x)
+    grad_y = cv.filter2D(gray, -1, y)
+
+    abs_grad_x = cv.convertScaleAbs(grad_x)
+    abs_grad_y = cv.convertScaleAbs(grad_y)    
+
+    # grad = cv.addWeighted(abs_grad_x, 0.7, abs_grad_y, 0.2, 0)
+    grad_mid = cv.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
+
+    flipped_img = cv.flip(grad_mid, 0)
+
+    return flipped_img
 
 
 img = cv.imread('C:\\opencv\\sources\\doc\\images\\lena.png')
@@ -60,14 +78,29 @@ y_roberts = np.array([
     [0, -1, 0]
 ]);
 
+# Module Derivate parcial x Prewittt
+x_prewit = np.array([
+    [-1, 0, 1],
+    [-1, 0, 1],
+    [-1, 0, 1]
+]);
+
+# Module Derivate parcial y Prewitt
+y_prewit = np.array([
+    [-1, -1, -1],
+    [0, 0, 0],
+    [1, 1, 1]
+]);
 
 grad = gradiant(x, y, img)
 grad_roberts = gradiant(x_roberts, y_roberts, img)
 grad_sobel = gradiant_sobel(img)
+grad_prewitt = gradiant_prewitt(x_prewit, y_prewit)
 
-cv.imshow('image sobel', grad_sobel)
-cv.imshow('image', grad)
-cv.imshow('image roberts', grad_roberts)
+cv.imshow('image prewitt', grad_prewitt)
+# cv.imshow('image sobel', grad_sobel)
+# cv.imshow('image', grad)
+# cv.imshow('image roberts', grad_roberts)
 
 while True:
     ch = cv.waitKey()
